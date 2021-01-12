1747617826889a2d4d2c662a60ae5e57af606493
myghim <myghim2@gmail.com>
2021-01-12 15:14:18 +0900

    Naver Shopping RankingFind VER0.3 - Naver Shopping 100 Ranking VER0.3  * 네이버 쇼핑 베스트 대분류 카테고리 10개에서 베스트 100 추출 마지막(10번) 면세점 카테고리 문제 해결

--- NaverShoppingRankingFind.py
+++ NaverShoppingRankingFind.py
@@ -15,7 +15,41 @@ import random
 # 스포츠/레저 : https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000007
 # 생활/건강 : https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000008
 # 여가/생활편의 : https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000009
+# 면세품 : https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000010
 
+
+
+
+# 패션의류, 패션잡화, 화장품/미용, 디지털/가전, 가구/인테리어, 출산/육아, 식품, 스포츠/레저, 생활/건강, 여가/생활편의
+lst_category = []
+"""for i in range(1, 10):
+    url = "https://search.shopping.naver.com/best100v2/detail.nhn?catId=" + str(50000000+i)
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'lxml')
+    metadata = soup.select('div.category_lst > div.summary_area > div > a.on')
+    category = metadata[0].text
+    lst_item = []
+
+    for j in range(1, 101):
+        time.sleep(random.randint(1, 3))
+        url = "https://search.shopping.naver.com/best100v2/detail.nhn?catId=" + str(50000000+i)
+        html = requests.get(url)
+        soup = BeautifulSoup(html.content, 'html.parser')
+        metadata_item = soup.select('#productListArea > ul > li:nth-child({}) > p > a'.format(j))
+        item = metadata_item[0].text
+        lst_item.append(item)
+        print(item)
+        #print(j," : ",item)
+    lst_length = []
+    for a in range(1,len(lst_item)+1):
+        lst_length.append(a)
+    rank_data = pd.DataFrame({"순위" : lst_length, "아이템명" : lst_item})
+    print(rank_data)
+    category = category.replace("/","")
+    rank_data.to_csv("C:/Users/onycom/Desktop/네이버베스트/네이버 베스트 100 {}.csv".format(category), encoding="cp949")
+"""
+
+# 면세품
 lst_category = []
 for i in range(10, 11):
     url = "https://search.shopping.naver.com/best100v2/detail.nhn?catId=" + str(50000000+i)
@@ -25,7 +59,7 @@ for i in range(10, 11):
     category = metadata[0].text
     lst_item = []
 
-    for j in range(39, 101):
+    for j in range(1, 40):
         time.sleep(random.randint(1, 3))
         url = "https://search.shopping.naver.com/best100v2/detail.nhn?catId=" + str(50000000+i)
         html = requests.get(url)
